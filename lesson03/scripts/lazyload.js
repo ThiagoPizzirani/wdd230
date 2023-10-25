3ac6fa528722123dc267b64e4e0dc6b2b566744f
ThiagoPizzirani <pizzthi@gmail.com>
2023-10-25 17:59:51 -0600

    BOM with local storage

---
+++ lesson03/scripts/lazyload.js
@@ -0,0 +1,5 @@
+document.getElementById("year").innerHTML = "&copy" + new Date().getFullYear();
+
+const date = document.lastModified;
+      const lastModified = new Date(date).toLocaleString();
+      document.getElementById("last-mod").innerHTML = "Last page modification: " + lastModified;
