d0413c5d4efdee13c87930e2c3d0cebfbf0b61cd
ThiagoPizzirani <pizzthi@gmail.com>
2023-10-25 18:07:04 -0600

    added page visits local storage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,17 @@ document.getElementById("year").innerHTML = "&copy" + new Date().getFullYear();
 const date = document.lastModified;
       const lastModified = new Date(date).toLocaleString();
       document.getElementById("last-mod").innerHTML = "Last Modification: " + lastModified;
+
+
+const visitsDisplay = document.querySelector("#page-visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = 'Page Visits: ' + numVisits;
+} else {
+	visitsDisplay.textContent = "Page Visits: First time, welcome!";
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
